f6de2ff64b5078669bc52718d8e5701cc49b8049
vietlong1011 <longlieuliny@gmail.com>
2023-05-18 14:42:42 +0700

    first commit

---
+++ src/main/java/com/example/rest_api_web_ban_hang_spring_boot/RestApiWebBanHangSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.rest_api_web_ban_hang_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiWebBanHangSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestApiWebBanHangSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/rest_api_web_ban_hang_spring_boot/RestApiWebBanHangSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rest_api_web_ban_hang_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiWebBanHangSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
